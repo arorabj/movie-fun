a1d9fc96cac81989cc706aedc4c7114efd065fe0
ashish8024571 <ashish.jain5@rbs.co.uk>
2019-09-10 11:11:34 +0530

    RabbitMQ

--- src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateMessageConsumer.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateMessageConsumer.java
@@ -2,6 +2,37 @@ package org.superbiz.moviefun.albums;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AlbumsUpdateMessageConsumer {
+
+
+    private final AlbumsUpdater albumsUpdater;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public AlbumsUpdateMessageConsumer(AlbumsUpdater albumsUpdater) {
+        this.albumsUpdater = albumsUpdater;
+    }
+
+
+    public void consume(Message<?> message) {
+        try {
+            logger.debug("Starting albums update");
+            albumsUpdater.update();
+            logger.debug("Finished albums update");
+
+        } catch (Throwable e) {
+            logger.error("Error while updating albums", e);
+        }
+    }
+}
+/*
+package org.superbiz.moviefun.albums;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -58,3 +89,4 @@ public class AlbumsUpdateScheduler {
         return updatedRows > 0;
     }
 }
+*/
